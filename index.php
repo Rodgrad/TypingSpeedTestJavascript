8e9baffe9131ef593cc0ae35148529e1bd94f6ea
Rodgrad <47482408+Rodgrad@users.noreply.github.com>
2020-08-16 00:37:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+
+<link type="text/css" id="light-mode" rel="stylesheet" href="css/base.css">
+<link href="https://fonts.googleapis.com/css2?family=Antic+Slab&display=swap" rel="stylesheet"> 
+
+
+<head>
+    <title>Typing Test</title>
+    <meta type='description' content="Typing speed test app. Test how fast you can type.">
+    <meta property="og:image" content="https://keyboardtypingtest.com/css/preview08.png"/>
+    <meta property="og:url" content="https://keyboardtypingtest.com/" />
+    <meta property="og:image:width" content="300" />
+    <meta property="og:image:height" content="300" />
+    <meta property="og:type" content="png" />
+    <meta property="og:title" content="KeyboardPro-Typing Speed Test" />
+    <meta property="og:description" content="Free app to test your typing speed, and to improve your skills with keyboard." />
+    <meta property="og:title" content="KeyboardPro-Typing Speed Test"/>
+    <meta name="og:description" content="Free app to test your typing speed, and to improve your skills with keyboard."/>
+</head>
+
+
+
+<body>
+
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0&appId=1228813847242354&autoLogAppEvents=1" nonce="KJF1Mrgl"></script>
+
+    <div id='wrapper'>
+        <div id='wrapper-stats'>
+           <header id='header'>
+               <h1 id='title'>Keyboard Typing Test</h1>
+               <p>How fast can you type? Test your skills right now.</p>
+           </header>
+           <div class='col-stats'>
+               <p class='stats-title'>WPM</p>
+               <div id='wpm'><p>0</p></div>
+           </div>
+           <div class='col-stats'>
+               <p class='stats-title'>CPM</p>
+               <div id='cpm'><p>0</p></div>
+           </div>
+           <div class='col-stats'>
+               <p class='stats-title'>WPM Accuracy</p>
+               <div id='wpm-accuracy-stats'><p>0.00%</p></div>
+           </div>
+           
+           <div class='col-stats'>
+               <p class='stats-title'>CPM Accuracy</p>
+               <div id='cpm-accuracy-stats'><p>0.00%</p></div>
+           </div>
+
+           <div class='col-stats'>
+               <p class='stats-title'>TIME</p>
+               <div id='time'><p>60s</p></div>
+           </div>
+        </div>
+
+        <div id='publish'>
+        
+            <div id='publish-stats'>
+                <br>
+                <br>
+                <div id='publish-msg'></div>
+                <div id='restart'><button  id='restart-btn'>Try again
+                </div>
+            </div>
+        </div>
+        
+        <div id="editor-section">
+            <p style='color:gray;'>Press Space to submit.</p>
+            <div id='editor-box' class='col-editor'><div id='editor'></div></div>
+            <div id='cursor-box' class='col-editor'><div id='cursor'> </div></div>
+            <div id='next-box' class='col-editor'><div id='next'></div></div>
+            <div class="fb-share-button" data-href="https://keyboardtypingtest.com" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fkeyboardtypingtest.com%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+
+        </div>
+
+        <br>
+        
+        <div id='info-section'>
+
+            <h2>All good reasons to learn type fast</h2>
+            <div class='col-info'><h3>Productivity</h3>           
+               <p>Save time, think how much of
+ a time you spend typing every day, what if you could cut that time by half?
+Speed typing skills will make it possible for you to finish tasks more quickly.
+ Have some extra time for yourself..</p>
+            </div>
+    
+            <div class='col-info'><h3>Energy</h3>           
+               <p>Stop looking at letters, 
+ your neck is under stress.
+At the end of the day are you dead tired? 
+ We can bet all the looking down to the keyboard and correcting errors have taken some of your energy. 
+ Stop doing extra work right now. Because you totally can
+ with little practice.
+                </p>
+            </div>
+    
+            <div class='col-info'><h3>Impressive</h3>  
+            <p>No matter what anyone says, 
+ fast typing is still an impressive skill to possess and necessary for some job positions.
+Nobody desire to look like it is his first time with a computer. And why would you, when you have everything you need to learn fast typing here.</p>
+            </div>
+    
+            <div class='col-info'><h3>Everything</h3>  
+                <p>If nothing rings a bell for you so far, we can bet you can find your reasons. 
+ If anything, typing is fun.</p>
+            </div>
+            <br>
+            <br>
+            <br>
+            <h2>How does it work?</h2>
+            <div class='col-info'><h3>WPM</h3>           
+               <p>WPM stands for words per minute. It is the total number of words you have typed correctly in the time frame of sixty seconds.
+It's calculated by dividing all correct character submissions sum with 5 over one minute. So in the sentence "This is mine." you will have two words. </p>
+            </div>
+            
+            <div class='col-info'><h3>CPM</h3>           
+               <p>CPM represents typed characters per minute, including errors.
+Every time you press a key of letter, numeral, or Space it will create an increment of one.</p>
+            </div>
+    
+            <div class='col-info'><h3>WPM Accuracy</h3>  
+            <p>Represents how accurate are your word submissions. 
+ Every time you submit a word, you will add one entry.
+And from that data, we calculate how accurate your submissions were depending on the correct and incorrect submission ratio.</p>
+            </div>
+    
+            <div class='col-info'><h3>CPM Accuracy</h3>  
+            <p>Represents how accurate are your character entries. Every time you press a key, you submit one entry.
+And from that data, we calculate how accurate your submissions were depending on correct and incorrect characters.</p>
+            </div>
+        </div>
+
+        <div id='about'>
+            <h2>About</h2>
+            <div class=col-about>
+                <h3>What is this app?</h3>
+                <p>Keyboard Typing Test is a free app 
+ designated to test your typing speed,
+ and to improve your skills with the keyboard.
+ Have fun typing. </p>
+            </div>
+            <div class=col-about>
+                <h3>Moto</h3>
+                <p>Practice, practice, practice.</p>
+            </div>
+        
+            <div class=col-about>
+                <h3>Team</h3>
+                <p>Luka Beslic,  developer. <br> For any questions, please contact me on <br>
+                   devluka.public@gmail.com</p>
+            </div>
+        
+        
+        </div>
+    </div>
+
+    <script type="text/javascript" src="js/main.js"></script>
+
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,419 @@
+/*
+System keys for exclusion
+*/
+var sysKeys = new Array(17, 32, 13, 18, 16, 20, 37, 38, 39, 40);
+var sysAllKeys = new Array(17, 8, 18, 16, 20, 37, 38, 39, 40);
+
+/*
+Holds text from file
+*/
+var textFile = new Array();
+/*
+Globals used for logic
+*/
+var wordIndex = 0;                               // Track word index                   
+var letterIndex = 0;                             // Track letter index
+var firstError = false;                          // Holds error bool
+var errorCounterLetter = 0;                      // Track letter errors after first error
+var cpmPoints =  0;                              // Track character pressed
+var cpmErrors = 0;                               // Track character pressed wrong errors
+var wpmPoints = 0;                               // Word submited  -valid
+var wpmErrors = 0;                               // Word submited  -invalid
+var currentWord = '';                            // Used to hold word to be written
+var backtrackWord = '';                          // Holds character values submited for [currentWord]
+var restartBool = true;                          // Used to disable/enable character counter, timer
+
+
+
+
+class Main{
+
+    
+    /*
+    Takes care ONLOAD START call to set app
+    */
+
+    startRound(){
+        restartBool = true;
+        set.setCursorBlinker();
+        set.setNewRound();
+
+    }
+
+
+    stopRound(){
+
+        this.collectStats();
+        set.setStatsStyle('col-stats', 'white', 'black');
+        set.setStatsStyle('stats-title', 'none', 'black');
+
+        restartBool = false;
+        document.getElementById('publish').style = "display:block;";
+
+    }
+    
+
+    /*
+    Called from [stopRound] to collect stats and create publish message to user
+    */
+    collectStats(){
+
+        var wpm =  progress.calculateWpmAccuracy();
+        // wpm[1]_ is percentage , wpm [0] is number count
+        var msg;
+        if(wpm[1] < 50 || wpm[0] < 25) {
+            msg = "You could do better, friend. Practice every day for 20 minutes. And drink coffe... or tea.";
+        }else if(wpm[1] < 70 && wpm[1] >= 50 || wpm[0] < 60 && wpm[0] >= 25){
+            msg = "You are doing pretty well. With little practice you could improve to professional.";
+        }
+        else if(wpm[1] < 80 && wpm[1] >= 70 || wpm[0] < 80 && wpm[0] >= 60){
+            msg = "You are crushing it, you are so close to become the legend.";
+        }
+        else if(wpm[1] > 70 &&  wpm[0] >= 80){
+            msg = "You are top of the class, true professional. You can do anything.";
+        }
+
+        set.setResultMsg(msg);
+   
+    }
+
+}
+
+
+
+class Setter{
+
+    /*
+    Setter class 
+    */
+
+    
+    setNewRound(){
+
+        if(textFile.length < 2){get.getText('words.txt');}
+
+
+        this.setStatsStyle('col-stats', 'none', ' #cecece');
+        set.setStatsStyle('stats-title', 'none', ' #a5bcff');
+
+        this.setNullGlobals();
+        get.getRandomized();
+        var getData = get.getTraverse(textFile, wordIndex);
+        set.setTraverse(getData[0], getData[1]);
+        document.getElementById('publish').style = "display:none;";
+        document.getElementById('editor').innerHTML = "";
+        document.getElementById('editor').innerHTML.trim();
+
+        document.getElementById('editor').style= "color:black;";
+
+
+
+    }
+
+    // set new word
+    setTraverse(word, words){
+
+            document.getElementById('next').innerHTML = "<span id='current'>"+ word +"</span>" +  words.join(' ').trim() ;
+    
+        }
+    
+    // reset values to null or equivalent
+    setNullGlobals(){
+        restartBool = true;
+        wordIndex = 0; 
+        letterIndex = 0; 
+        cpmPoints =  0;
+        cpmErrors = 0;
+        wpmPoints = 0;
+        wpmErrors = 0;
+        currentWord = ''; 
+        backtrackWord = '';
+        firstError = false;
+        document.getElementById('wpm').innerHTML="<p>" + 0 + " </p>";
+        document.getElementById('cpm').innerHTML="<p>" + 0 + " </p>";
+        document.getElementById('wpm-accuracy-stats').innerHTML="<p>" + '0.00' + "%</p>";
+        document.getElementById('cpm-accuracy-stats').innerHTML="<p>" + '0.00' + "%</p>";
+        document.getElementById('time').innerHTML="<p>" + 60 + "s</p>";
+
+    }
+
+    // Cursor blink, text write guide
+    setCursorBlinker(){
+
+        var state;
+        setInterval(function(){
+            if(state == 1){
+                document.getElementById('cursor').style = 'display:block';
+                state=0;
+            }else{
+                document.getElementById('cursor').style = 'display:none';
+                state=1;
+            }
+        }, 1000);
+    }
+
+    // Publish selected message from Main[collectStats]
+    setResultMsg(msg){
+        var index = 0;
+        var word = '';
+        let a = setInterval(function(){
+            word = word + msg[index];
+            document.getElementById('publish-msg').innerHTML = word;
+            index++;
+            if(index == msg.length || restartBool == true){
+                document.getElementById('publish-msg').style = "    text-decoration: underline";
+
+                clearInterval(a);
+            }
+        }, 30);
+    }
+
+    setStatsStyle(element, property1, property2){
+
+        let elem = document.getElementsByClassName(element);
+        for(var i=0; i<elem.length; i++){
+            elem[i].style = "background:" + property1 + "; transition-duration:1s; color:" + property2 +  "; ";
+        }
+    }
+}
+
+
+
+class Getter{
+
+    /*
+    Get any data necessary 
+    */
+
+    // Awaits for user event 
+    getInput(){
+
+        if (!restartBool){return false;}
+
+        if(!sysAllKeys.includes(event.keyCode)){
+            ++cpmPoints;
+        }
+        if (cpmPoints == 1){
+            timer.start_time();    
+        }
+        var getWord = get.getTraverse(textFile, wordIndex);
+
+        // Word submit event
+        if (event.keyCode == 32 || event.keyCode == 13 ){
+            event.preventDefault();
+            progress.validateWord(backtrackWord, getWord[0]);
+            progress.calculateWpmAccuracy();
+
+            wordIndex++;        
+            getWord = get.getTraverse(textFile, wordIndex);
+            set.setTraverse(getWord[0], getWord[1]);
+            backtrackWord = '';
+            currentWord = '';
+            letterIndex = 0;
+            errorCounterLetter = 0;
+            document.getElementById('editor').innerHTML = "";
+            firstError = false;
+
+
+        }
+
+        // Leter submit event
+        if (!sysKeys.includes(event.keyCode)){
+            var letter = String.fromCharCode(event.keyCode).toLowerCase();
+            event.preventDefault();
+            progress.validateLetter(getWord[0], letter);
+        }
+
+
+        progress.calculateCpmAccuracy();
+        document.getElementById('cpm').innerHTML="<p>" + cpmPoints + "</p>"
+
+    }
+
+    // GET next word
+    getTraverse(file, index){
+
+        var word = file[index].trim();
+        file = file.slice(index+1, file.length);
+        return [word , file];
+    }
+
+    // Randomize words for new round
+    getRandomized(){
+
+        var j, x, i;
+        for (i = textFile.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1)) ;
+            x = textFile[i].toLowerCase();
+            textFile[i] =   textFile[j] ;
+            textFile[j] = x;
+        }
+
+
+    }
+
+    // Get words from file , this is intended to be called only once, but if [textFile] is empty it will call again from SETTER
+    getText(file){
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                textFile  =  this.responseText.split("\n");
+            }
+        };
+        xhttp.open("GET", file, false);
+        xhttp.send();
+    }
+
+}
+
+
+
+class Progression{
+
+    /*
+    Validate user inputs and outputs result 
+    */
+
+
+    validateLetter(word, letter){
+        // IF Backspace
+        console.log(word[letterIndex], " letIx ",  letterIndex , "   letixnum", letter, "  let",  firstError, " first err");
+        if(event.keyCode == 8){
+            if(errorCounterLetter > 0 ){
+                 errorCounterLetter--;}
+            if(letterIndex > 0){
+            letterIndex--;}
+            if(errorCounterLetter < 1 && word[letterIndex] == backtrackWord[letterIndex] && letterIndex >= 0 && backtrackWord ){
+                currentWord = backtrackWord[letterIndex] + currentWord;
+                firstError = false;
+            }
+            backtrackWord = backtrackWord.slice(0, letterIndex);
+            if(backtrackWord.length < 1 || backtrackWord == word.slice(0, letterIndex) && letterIndex >=0 ){
+                firstError = false;
+                document.getElementById('editor').style= "color:black;";
+            }
+            if(backtrackWord == word || backtrackWord.length > word.length){
+                currentWord = '';
+            }
+
+            document.getElementById('current').innerHTML = currentWord;
+          
+        }// Letter character entry
+        // IF correct letter
+        else if(event.keyCode != 8){
+            if(word[letterIndex] == letter && !firstError){
+                backtrackWord = backtrackWord + letter;
+                currentWord  = word.substring(letterIndex+1);
+                letterIndex++;
+                document.getElementById('current').innerHTML = currentWord;
+                document.getElementById('editor').style= "color:black;";
+
+
+            }
+            // IF uncorrect letter
+            else{
+                cpmErrors++;
+                letterIndex++;
+                backtrackWord = backtrackWord + letter;
+                document.getElementById('editor').style= "color:red;";
+                firstError = true;
+                errorCounterLetter++;
+                if(currentWord == ''){
+                    currentWord = word;
+                }
+             
+            }
+        }
+
+        document.getElementById('editor').innerHTML = backtrackWord;
+    }
+
+    calculateCpmAccuracy(){
+
+        var result = (100 - ((cpmErrors / (cpmPoints))  * 100)).toFixed(2);   // we do not need to add cpmPoints + cpmErrors because it cpmPoints alread contain errors
+        document.getElementById('cpm-accuracy-stats').innerHTML = "<p>" +  result  + "%</p>";
+        return result;
+    
+    }
+
+
+    validateWord(input, word){
+
+            //case empty submision, count whole current word
+            if(backtrackWord.length < 1){
+                ++cpmPoints;
+                ++cpmErrors;
+            }
+
+            if (input == textFile[wordIndex]){
+                wpmPoints = wpmPoints + input.length ;
+
+            }else{
+                wpmErrors = wpmErrors + textFile[wordIndex].length ;
+                --cpmPoints; // noramlize space as wrong step
+            }
+            
+
+        
+    }
+
+    calculateWpmAccuracy(){
+
+        var result1 = Math.round( wpmPoints / 5 );
+        document.getElementById('wpm').innerHTML = "<p>" + result1 +"</p>";
+        if (wpmPoints < 1 || wpmPoints < 1 ){
+            ++wpmErrors;
+        }
+        var result2 = (100 - (( wpmErrors / ( wpmPoints + wpmErrors))   * 100)).toFixed(2); 
+        document.getElementById("wpm-accuracy-stats").innerHTML = "<p>" + result2 + "%</p>";
+        return [result1, result2];
+    }
+
+
+
+}
+
+
+class Timer{
+
+    start_time(){
+        var seconds = 60;
+        var timer = setInterval(function(){    
+            if (seconds == 50){
+                clearInterval(timer);
+                main.stopRound();
+            }
+            document.getElementById('time').innerHTML = "<p>" + (seconds--) + "s</p>";}, 1000);  
+     }
+}
+
+
+
+
+
+
+const get = new Getter();
+const set = new Setter();
+const main = new Main();
+const progress = new Progression();
+const timer = new Timer();
+window.onload = main.startRound(), document.hasFocus();
+
+
+// User input event handler
+document.addEventListener('keydown', event => {
+    get.getInput(event);
+
+
+});
+
+
+document.getElementById('restart-btn').addEventListener('click', event => {
+    set.setNewRound();
+
+});
+
+if(window.outerWidth < 800){
+    alert("Site is not supported for mobile devices, please visit us over desktop.");
+}
